f1dcac11c52ee66b25f56f7fe2f9899a9dc06f6d
nicolas_macbook <noriega.nicolas@gmail.com>
2022-04-22 21:15:53 -0400

    adds baseline and metrics

--- Project/train.py
+++ Project/train.py
@@ -1,19 +1,22 @@
+from tkinter import N
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm, trange
-
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import pairwise_distances
+from sklearn.decomposition import PCA
 from mimic import Mimic3
 from model import PatientSimiEval
 from siamese_cnn import SiameseCNN
 from utils import pairwise_intersect
 from word2vec import SkipGramDataset, Word2Vec
-
+from datetime import datetime
 
 # constants
-DATASET_PATH = 'Dataset/all_files/'
+DATASET_PATH = '../../../Project/Dataset/all_files/'
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
@@ -105,3 +108,71 @@ for epoch in trange(scnn_epochs, desc='Training PSE'):
         
         optim.step()
         optim.zero_grad()
+        
+        
+        
+        
+
+# TODO create validation set, and evalute
+
+
+# most similar patients:
+reprs = [pse_model(codes).detach().cpu().numpy() for codes, _ in data_loader]
+reprs = np.vstack(reprs)
+
+pdist =  cosine_similarity(reprs)
+np.fill_diagonal(pdist, -2)
+nearest_pse = pdist.argmax(axis=1)
+
+
+# BASELINES
+codes_onehot = np.zeros((len(dataset), dataset.num_codes()), dtype=np.int32)
+for i, (codes, _) in enumerate(dataset):
+    codes[i, codes] = 1
+    
+# PCA: pca of one-hode encoded codes
+pca = PCA(n_components=out_dim) #  cohorts
+codes_pca = pca.fit_transform(codes_onehot) # (N, 9)
+pdist_pca = pairwise_distances(codes_pca) # pairwise euclidean dist
+np.fill_diagonal(pdist_pca, np.inf) # ignore the diaginals
+nearest_pca = pdist_pca.argmin(axis=1) # closest patients based on PCA
+
+# Hamming distance:
+pdist_hamming = pairwise_distances(codes_onehot, metirc='hamming')
+np.fill_diagonal(pdist_hamming, np.inf) # ignore the diaginals
+nearest_hamming = pdist_hamming.argmin(axis=1) # closest patients based on PCA
+
+
+
+# The Hospital Readmission Rate (HRR)
+nadmin = dataset.info['NADMISSIONS'].to_numpy()
+def hospital_readmin_rate(nearest):
+    return ((nadmin[nearest] == 1) & (nadmin == 1)) | ((nadmin[nearest] > 1) & (nadmin > 1)).mean()
+
+# Incidence Rate Difference for Mortality (IRDM)
+dischtime = dataset.info['DISCHTIME']
+deathtime = dataset.info['DEATHTIME']
+
+def incidence_rate(dischtime, deathtime):
+    death_ct = (~deathtime.isna()).sum()
+    diff = (deathtime.fillna(datetime(2022, 1, 1)) - dischtime).dt.days
+    return death_ct / diff
+
+def inc_rate_mortality(nearest):
+    ir_p = incidence_rate(dischtime, deathtime)
+    ir_sp = incidence_rate(dischtime.iloc[nearest], deathtime.iloc[nearest])
+    return np.abs(ir_p - ir_sp)
+
+
+
+methods = {
+    "PSE": nearest_pse,
+    "PCA": nearest_pca,
+    "Hamming": nearest_hamming
+}
+
+irdm = {}
+hrr = {}
+for name, nearest in methods.items():
+    irdm[name] = inc_rate_mortality(nearest)
+    hrr[name] = hospital_readmin_rate(nearest)
